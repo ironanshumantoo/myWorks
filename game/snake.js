5305bfadcda3903b864fe8fc21600f4a388ec2d6
anshuman <yellowmamba43@gmail.com>
2020-05-12 10:33:02 +0530

    almost complete snake game

--- game/snake.js
+++ game/snake.js
@@ -1,8 +1,16 @@
 var canvas=document.querySelector('canvas');
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+canvas.width=window.innerWidth-2;
+canvas.height=window.innerHeight-4;
 var c=canvas.getContext("2d");
 
+//for alighment of food and snake
+var w=Math.floor(window.innerWidth/20)*20;
+var h=Math.floor(window.innerHeight/20)*20;
+var pixelx=Math.floor(w/200);
+var pixely=Math.floor(h/200);
+
+
+//var newbody=[];
 function Snake(body){
     this.body=body;
     this.direction=1;
@@ -16,10 +24,22 @@ function Snake(body){
         this.x=cx;
         this.y=cy;
     }
-   
-   this.addcomp=function(ax,ay){
-       this.body.push(new this.component(ax,ay));
+    this.addcomp=function(ax,ay){
+        this.body.push(new this.component(ax,ay));
+    }
+    this.death=function(){
+        for(var i=1;i<this.body.length;i++)
+        {
+            if(Math.abs(this.body[0].x-this.body[i].x)<=10  && Math.abs(this.body[0].y-this.body[i].y)<=10 )  
+            this.destroyed();
+        }
+    }
+   this.destroyed=function(){
+       var l=this.body.length;
+       for(var i=1;i<l;i++)
+       this.body.pop();
    }
+   
  
    this.update=function(){
     if(this.direction==1)
@@ -30,10 +50,17 @@ function Snake(body){
     this.body[0].y-=20;
     if(this.direction==4)
     this.body[0].y+=20;
-    if(this.body[0].x>canvas.width)
+    if(this.body[0].x>window.innerWidth)
     this.body[0].x=0;
-    if(this.body[0].y>canvas.height)
+    if(this.body[0].x<0)
+    this.body[0].x=w;
+    if(this.body[0].y<0)
+    this.body[0].y=h;
+    if(this.body[0].y>window.innerHeight)
     this.body[0].y=0;
+    
+     this.death();
+    //update
        for(var i=this.body.length-1;i>=1;i--)
        {this.body[i].x=this.body[i-1].x;
         this.body[i].y=this.body[i-1].y;
@@ -47,20 +74,20 @@ function Snake(body){
   
 }
 
-function Food(x,y){
-    this.x=x;
-    this.y=y;
+function Food(fx,fy){
+    this.x=fx;
+    this.y=fy;
     this.draw=function(){
-        c.fillStyle='white';
+        c.fillStyle='red';
         c.fillRect(this.x,this.y,15,15);
     }
 }
 
 var snakeBody=[];
 var snake=new Snake(snakeBody);
-snake.addcomp(50,100);
-snake.addcomp(50,120);
-snake.addcomp(50,140);snake.addcomp(50,160);snake.addcomp(50,180);
+snake.addcomp(60,100);
+snake.addcomp(80,120);
+
 
 var fx=Math.random()*canvas.width;
 var fy=Math.random()*canvas.height;
@@ -69,13 +96,13 @@ eat.draw();
 
 window.addEventListener('keypress',dChange,false);
 function dChange(e){
-    if(e.keyCode==54)
+    if(e.keyCode==54&&snake.direction!=2)
     snake.direction=1;
-    if(e.keyCode==52)
+    if(e.keyCode==52&&snake.direction!=1)
     snake.direction=2;
-    if(e.keyCode==50)
+    if(e.keyCode==50&& snake.direction!=3)
     snake.direction=4;
-    if(e.keyCode==56)
+    if(e.keyCode==56&&snake.direction!=4)
     snake.direction=3;
     console.log('a');
 
@@ -93,11 +120,24 @@ function startAnimating(fps) {
     animate();
 }
 
-
-
 function animate(){
     requestAnimationFrame(animate);
-    
+    //calculting distance
+    //console.log(eat.x);
+    if(Math.abs( snake.body[0].x-eat.x)<=15&&Math.abs(snake.body[0].y-eat.y)<=15) 
+     {
+          var length=snake.body.length-1;
+         snake.addcomp(snake.body[length].x,snake.body[length].y  );
+         fx=Math.floor(Math.random()*10)*20*pixelx;
+         fy=Math.floor(Math.random()*10)*20*pixely;
+         console.log(fx);
+         console.log(fy);
+         eat=new Food(fx,fy);
+         
+     }
+     //time
+
+
     now = Date.now();
     elapsed = now - then;
 
@@ -113,6 +153,7 @@ function animate(){
     c.clearRect(0,0,canvas.width,canvas.height);
     
     snake.update();
+    eat.draw();
     }
   
    
